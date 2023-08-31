9e702457d88ae005618fb4c95242a8e2d8d3e6bb
devluanchristian <luan_christian-r@hotmail.com>
2023-08-30 22:26:18 -0300

    correção

--- pratica-01-api-para-exibir-bancos/src/models/bankModel.js
+++ pratica-01-api-para-exibir-bancos/src/models/bankModel.js
@@ -5,8 +5,15 @@ class banks {
   getBank(callback) {
     fs.readFile("./data/banco.json", "utf8", function (err, result) {
       if (!err) {
-        callback(err, result);
+        var obj = JSON.parse(result);
       }
+      obj.banks.forEach(function (bank) {
+        if (i >= 0) {
+          data[i] = bank;
+          i--;
+        }
+      });
+      callback(err, result);
     });
   }
 }
